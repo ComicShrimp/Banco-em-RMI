7fe5d3191b0ece314cfcad78b64ad1706a0875f9
Mário Victor Ribeiro Silva <mariovictorrs@gmail.com>
2019-11-18 20:05:15 -0300

    Algumas refatorações
    Melhorando a identação e aplicando alguns principios de clean code

--- src/BankService.java
+++ src/BankService.java
@@ -4,90 +4,86 @@ import java.util.ArrayList;
 
 public class BankService extends UnicastRemoteObject implements Bank {
 
-    java.util.ArrayList<User> database = new ArrayList<>();
-    String[] numbers = { "5581 7461 6024 0275", "5171 0511 6130 1176", "5484 2803 4769 6777", "5580 6278 6824 7815",
-            "5314 4726 2902 1966" };
+  java.util.ArrayList<User> database = new ArrayList<>();
+  String[] numbers = { "5581 7461 6024 0275", "5171 0511 6130 1176", "5484 2803 4769 6777", "5580 6278 6824 7815",
+      "5314 4726 2902 1966" };
 
-    private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
 
-    public BankService() throws RemoteException {
+  public BankService() throws RemoteException {
 
-        for (int i = 0; i < 5; i++) {
-            User u = new User();
-            u.setCardNumber(numbers[i]);
-            u.setSale(100 * i);
-            database.add(u);
-        }
+    for (int i = 0; i < 5; i++) {
+      User newUser = new User(numbers[i], 100 * i);
+      database.add(newUser);
+    }
+  }
+
+  public boolean getAccount(String token) throws RemoteException {
+    for (User user : database) {
+      if (user.getCardNumber().equals(token)) {
+        return true;
+      }
     }
 
-    public boolean getAccount(String token) throws RemoteException {
-        for (User u : database) {
-            if (u.getCardNumber().equals(token)) {
-                return true;
-            }
-        }
+    return false;
+  }
 
-        return false;
-    }
+  public double makeWithdraw(String tokenId, double value) throws RemoteException {
+    double takenOutValue = 0.0f;
+    for (User user : database) {
+      if (user.getCardNumber().equals(tokenId)) {
 
-    public double makeWithdraw(String tokenId, double value) throws RemoteException {
-        double takenOutValue = 0.0f;
-        for (User u : database) {
-            if (u.getCardNumber().equals(tokenId)) {
-
-                if (u.getSale() < value) {
-                    return 0.0f;
-                } else {
-                    takenOutValue = u.getSale() - value;
-                    u.setSale((double) (u.getSale() - value));
-                    return takenOutValue;
-                }
-
-            }
-        }
-
-        return takenOutValue;
-    }
-
-    public void makeDeposit(String tokenId, double value) throws RemoteException {
-        for (User u : database) {
-            if (u.getCardNumber().equals(tokenId)) {
-                u.setSale(u.getSale() + value);
-                break;
-            }
-        }
-    }
-
-    public boolean makeTransfer(String fromUser, String toUser, double value) throws RemoteException {
-        int fi = null, ti = null;
-        for (User u : database) {
-            if (u.getCardNumber().equals(fromUser)) {
-                fi = database.indexOf(u);
-            }
-
-            if (u.getCardNumber().equals(toUser)) {
-                ti = database.indexOf(u);
-            }
-        }
-
-        if (database.get(fi).getSale() < value) {
-            return false;
+        if (user.getSale() < value) {
+          return 0.0f;
         } else {
-            database.get(fi).setSale(database.get(fi).getSale() - value);
-            database.get(ti).setSale(database.get(ti).getSale() + value);
-            return true;
+          takenOutValue = user.getSale() - value;
+          user.setSale((double) (user.getSale() - value));
+          return takenOutValue;
         }
 
-        return false;
+      }
     }
 
-    public void getSale(String tokenId) throws RemoteException {
-        for (User u : database) {
-            if (u.getCardNumber().equals(tokenId)) {
-                System.out.println(u.getSale());
-                break;
-            }
-        }
+    return takenOutValue;
+  }
+
+  public void makeDeposit(String tokenId, double value) throws RemoteException {
+    for (User user : database) {
+      if (user.getCardNumber().equals(tokenId)) {
+        user.setSale(user.getSale() + value);
+        break;
+      }
     }
+  }
+
+  public boolean makeTransfer(String fromUser, String toUser, double value) throws RemoteException {
+    int fi = 0, ti = 0;
+    for (User user : database) {
+      if (user.getCardNumber().equals(fromUser)) {
+        fi = database.indexOf(user);
+      }
+
+      if (user.getCardNumber().equals(toUser)) {
+        ti = database.indexOf(user);
+      }
+    }
+
+    if (database.get(fi).getSale() < value) {
+      return false;
+    } else {
+      database.get(fi).setSale(database.get(fi).getSale() - value);
+      database.get(ti).setSale(database.get(ti).getSale() + value);
+      return true;
+    }
+  }
+
+  public void getSale(String tokenId) throws RemoteException {
+    for (User user : database) {
+      if (user.getCardNumber().equals(tokenId)) {
+        System.out.println(user.getSale());
+        break;
+      }
+    }
+  }
 
 }
--- src/User.java
+++ src/User.java
@@ -1,37 +1,44 @@
 import java.io.Serializable;
 
-public class User implements Serializable{
-    /**
-     *
-     */
-    private static final long serialVersionUID = 1L;
-    private String cardNumber;
-    private double sale;
-
-    public String getCardNumber() {
-        return cardNumber;
-    }
-
-    public double getSale() {
-        return sale;
-    }
-
-    public void setSale(double sale) {
-        this.sale = sale;
-    }
-
-    public void setCardNumber(String cardNumber) {
-        this.cardNumber = cardNumber;
-    }
-
-=======
-public class User {
+public class User implements Serializable {
+  // Atributos
+  private static final long serialVersionUID = 1L;
+  private String cardNumber;
+  private double sale;
   String nome;
   Double saldo;
 
-  public User (String nome, Double saldo) {
-    this.nome = nome;
-    this.saldo = saldo;
+  // Construtor tipo 1
+  public User(String Nome, Double Saldo) {
+    this.nome = Nome;
+    this.saldo = Saldo;
+  }
+
+  // Construtor Tipo 2
+  public User(String CardNumber, double Sale) {
+    this.cardNumber = CardNumber;
+    this.sale = Sale;
+  }
+
+  public User() {
+
+  }
+
+  // Getter e Setters
+  public String getCardNumber() {
+    return cardNumber;
+  }
+
+  public double getSale() {
+    return sale;
+  }
+
+  public void setSale(double sale) {
+    this.sale = sale;
+  }
+
+  public void setCardNumber(String cardNumber) {
+    this.cardNumber = cardNumber;
   }
 
   public String getNome() {
@@ -49,5 +56,4 @@ public class User {
   public void setSaldo(Double saldo) {
     this.saldo = saldo;
   }
->>>>>>> 2abe046e6493b2e4ab50fdb25048a26b6e4e7755
 }
