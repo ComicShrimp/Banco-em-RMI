66f537f683da6f522c90cc726a8e51ca072eae82
Mário Victor Ribeiro Silva <mariovictorrs@gmail.com>
2019-11-19 15:15:06 -0300

    Adiciona mais um Banco

---
+++ src/Bank3Service.java
@@ -0,0 +1,160 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.IOException;
+
+public class Bank3Service extends UnicastRemoteObject implements Bank {
+
+  ArrayList<User> database = new ArrayList<>();
+  private static final String FILE_HEADER = "Name,CardNumber,Password,BankName,Saldo";
+
+  private static final long serialVersionUID = 1L;
+
+  String name = "Bradesco";
+
+  public Bank3Service() throws RemoteException {
+    File baseDados = new File("../src/bases/Banco2.csv");
+
+    String[] campos = {};
+    try {
+      Scanner leitor = new Scanner(baseDados);
+
+      leitor.nextLine();
+
+      while (leitor.hasNext()) {
+        String l = leitor.nextLine();
+        campos = l.split(",");
+        User u = new User();
+        u.setNome(String.valueOf(campos[0]));
+        u.setCardNumber(String.valueOf(campos[1]));
+        u.setPassword(String.valueOf(campos[2]));
+        u.setNomeBanco(String.valueOf(campos[3]));
+        u.setSaldo(Double.valueOf(campos[4]));
+        database.add(u);
+      }
+
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    }
+
+  }
+
+  public int getAccount(String token) throws RemoteException {
+
+    for (User u : database) {
+      if (u.getCardNumber().equals(token)) {
+        return database.indexOf(u);
+      }
+    }
+
+    return -1;
+  }
+
+  /**
+   * @return the name
+   */
+  public String getName() {
+    return name;
+  }
+
+  public void writeCsv() {
+    FileWriter fileWriter = null;
+
+    try {
+      fileWriter = new FileWriter("../src/bases/Banco2.csv");
+      fileWriter.append(FILE_HEADER.toString());
+      fileWriter.append("\n");
+
+      for (User u : database) {
+        fileWriter.append(u.getNome());
+        fileWriter.append(",");
+        fileWriter.append(u.getCardNumber());
+        fileWriter.append(",");
+        fileWriter.append(u.getPassword());
+        fileWriter.append(",");
+        fileWriter.append(u.getNomeBanco());
+        fileWriter.append(",");
+        fileWriter.append(String.valueOf(u.getSaldo()));
+        fileWriter.append("\n");
+      }
+
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+    } finally {
+      try {
+        fileWriter.flush();
+        fileWriter.close();
+      } catch (IOException e) {
+        System.out.println("Error while flushing/closing fileWriter !!!");
+        e.printStackTrace();
+      }
+
+    }
+  }
+
+  public double makeWithdraw(String tokenId, String password, double value) throws RemoteException {
+    double takenOutValue = 0.0f;
+    for (User user : database) {
+      if (user.getCardNumber().equals(tokenId)) {
+        if (user.getSaldo() < value) {
+          return 0.0f;
+        } else {
+          takenOutValue = user.getSaldo() - value;
+          user.setSaldo((double) (user.getSaldo() - value));
+          writeCsv();
+          return takenOutValue;
+        }
+
+      }
+    }
+
+    return 0.0f;
+  }
+
+  public void makeDeposit(String tokenId, String password, double value) throws RemoteException {
+    for (User user : database) {
+      if (user.getCardNumber().equals(tokenId)) {
+        user.setSaldo(user.getSaldo() + value);
+        writeCsv();
+        break;
+      }
+    }
+  }
+
+  public boolean makeTransfer(String fromUser, String toUser, String password, double value) throws RemoteException {
+    int fi = 0, ti = 0;
+    for (User user : database) {
+      if (user.getCardNumber().equals(fromUser)) {
+        fi = database.indexOf(user);
+      }
+
+      if (user.getCardNumber().equals(toUser)) {
+        ti = database.indexOf(user);
+      }
+    }
+
+    if (database.get(fi).getSaldo() < value) {
+      return false;
+    } else {
+      database.get(fi).setSaldo(database.get(fi).getSaldo() - value);
+      database.get(ti).setSaldo(database.get(ti).getSaldo() + value);
+      writeCsv();
+      return true;
+    }
+
+  }
+
+  public void getSale(String tokenId) throws RemoteException {
+    for (User user : database) {
+      if (user.getCardNumber().equals(tokenId)) {
+        System.out.println(user.getSaldo());
+        break;
+      }
+    }
+  }
+
+}
--- src/Client.java
+++ src/Client.java
@@ -8,16 +8,17 @@ public class Client {
   public static void main(String[] args) {
 
     try {
-      Bank bd = (Bank) Naming.lookup("//127.0.0.1:1099/BancoService");
-      bd.getAccount("5171 0511 6130 1176");
-      ///bd.makeTransfer("5171 0511 6130 1176", "5581 7461 6024 0275", 100.00);
+      Bank bank1 = (Bank) Naming.lookup("//127.0.0.1:1099/BancoService");
+
+      Bank bank2 = (Bank) Naming.lookup("//127.0.0.1:1099/BancoService2");
+
+      Bank bank3 = (Bank) Naming.lookup("//127.0.0.1:1099/BancoService3");
+
+      /// bd.makeTransfer("5171 0511 6130 1176", "5581 7461 6024 0275", 100.00);
       // bd.makeDeposit("5171 0511 6130 1176", 200.00);
-    
-    
-      //  Bank st = (Bank) Naming.lookup("//127.0.0.1:1099/BancoService2");
-    //  System.out.println(st.getName());
 
-
+      // Bank st = (Bank) Naming.lookup("//127.0.0.1:1099/BancoService2");
+      // System.out.println(st.getName());
 
     } catch (Exception e) {
       e.printStackTrace();
