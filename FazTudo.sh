b98f250631e4073b46f8f047b7be7e45fdc9d327
Mário Victor Ribeiro Silva <mariovictorrs@gmail.com>
2019-11-19 15:19:56 -0300

    Atualiza o .sh

--- FazTudo.sh
+++ FazTudo.sh
@@ -3,11 +3,5 @@
 # Compila todos os arquivos
 javac src/*.java -d Executaveis
 
-# Compila rmic
-rmic Executaveis/*Service.class
-
-# Faz o registro
-rmiregistry
-
 # Aviso
 echo "Executar o servidor e cliente manualmente"
